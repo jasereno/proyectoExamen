8d49f3affa5500f5c71127973916061c237732b1
chinoowo <di.croma@duocuc.cl>
2023-05-14 20:58:49 -0400

    jquery animacion

---
+++ js/animacion.js
@@ -0,0 +1,12 @@
+$("#ocultar").on("click", function () {
+    //$(".primero").hide();
+    //$(".primero").fadeOut();
+    $("#primero").slideUp();
+})
+
+$("#mostrar").on("click", function () {
+    //$(".primero").show();
+    //$(".primero").fadeIn();
+    $("#primero").slideDown();
+})
+
